f82d683cd5eb95c3a49d14aa1eb81b3108e354b0
Videshyn <maxim.videshyn@gmail.com>
2017-07-03 13:48:15 +0300

    necessary corrections

--- Filter/app/src/main/java/com/example/vedia/filter/MainActivity.java
+++ Filter/app/src/main/java/com/example/vedia/filter/MainActivity.java
@@ -5,47 +5,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button change;
-    EditText edit;
-    final String FORMER = "***";
+    private Button buttonChange;
+    private EditText edit;
+    public final String FORMER = "***";
+    public final String CENZURE = "(?i)(х[у,е][р,й,я,ю,и,е,ё]|п[и,е,ё]зд|^[ё,е]б|пид[^ж]|чмыр|залуп|бляд|[вы,у,по]" +
+            "[ё,е]б|за[ё,е]б|конч)";
+    public final String SPACE = " ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        change = (Button) findViewById(R.id.btnFilter);
+        buttonChange = (Button) findViewById(R.id.btnFilter);
         edit = (EditText) findViewById(R.id.edit);
 
-        change.setOnClickListener(this);
+        buttonChange.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        String str = String.valueOf(edit.getText());
-        Pattern pattern = Pattern.compile("(?i)(х[у,е][р,й,я,ю,и,е,ё]|п[и,е,ё]зд|^[ё,е]б|пид[^ж]|чмыр|залуп|бляд|[вы,у,по]" +
-                "[ё,е]б|за[ё,е]б|конч)");
-        StringBuilder res = replace(pattern, str);
-        edit.setText(res);
+        String editString = edit.getText().toString();
+        Pattern pattern = Pattern.compile(CENZURE);
+        String resultString = replace(pattern, editString);
+        edit.setText(resultString);
     }
 
-    public StringBuilder replace(Pattern pattern, String editStr){
-        StringBuilder res = new StringBuilder("");
-        String[] arr = editStr.split(" ");
+    public String replace(Pattern pattern, String editStr){
+        String resultString = "";
+        String[] arr = editStr.split(SPACE);
         for (int i = 0, n = arr.length; i < n; i ++){
-            Matcher matcher = pattern.matcher(editStr);
+            Matcher matcher = pattern.matcher(arr[i]);
             if (matcher.find()){
                 arr[i] = FORMER;
             }
-            res.append(arr[i] + " ");
+            resultString = arr[i] + SPACE;
         }
-        return res;
+        return resultString;
     }
 }
