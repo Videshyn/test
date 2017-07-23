4cb231f37bcc29f7772038f3c3655706a4e4b72e
Videshyn <maxim.videshyn@gmail.com>
2017-07-24 02:01:24 +0300

    firebase test. Authentication

---
+++ MyAdm/app/src/main/java/com/example/user/myadm/SecindActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.myadm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by User on 24.07.2017.
+ */
+
+public class SecindActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
